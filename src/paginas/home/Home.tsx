be92801dc7f9c5089416bd951dd0acd118d583f9
Catharina Vollmers <guilhermerivascontato@gmail.com>
2022-01-17 10:14:12 -0300

    Pagina home

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className='titulo'>Home</h1>
+            <img  src="https://images4.fanpop.com/image/photos/23500000/Seth-MacFarlane-s-Cavalcade-of-Cartoon-Comedy-Cat-Staff-Meeting-seth-macfarlane-23593214-1360-768.jpg" alt="Reunião" className='img' />
+        </>
+    );
+}
+
+export default Home
