9fdb185b1384bbffdd3a50c27935eee9d5166cc7
GuilhermeRivas <guilhermerivascontato@gmail.com>
2022-01-17 18:48:44 -0300

    Navbar, Home e Footer adicionado.

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import {Typography, Box, Grid, Button} from '@material-ui/core';
 import './Home.css';
 
-function Home(){
-    return(
+function Home() {
+    return (
         <>
-            <h1 className='titulo'>Home</h1>
-            <img  src="https://images4.fanpop.com/image/photos/23500000/Seth-MacFarlane-s-Cavalcade-of-Cartoon-Comedy-Cat-Staff-Meeting-seth-macfarlane-23593214-1360-768.jpg" alt="Reunião" className='img' />
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
         </>
     );
 }
 
-export default Home
+export default Home;
